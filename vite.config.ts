3d84b5b1e61d070d68847b9dae991e8d4a56f88c
pipinstallaadit <appujari80@gmail.com>
2025-11-04 16:18:10 +0530

    deploy error fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,15 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  // 👇 ADD THIS LINE — set your repo name here
+  base: "/Aadit-Portfolio/", 
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
